6d3f6006e694d0a90003df375c4c6cea4ccbf016
HariVP03 <harryskotch11@gmail.com>
2022-08-13 22:29:08 +0530

    init

---
+++ apps/github-actions-test-lol/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="github-actions-test-lol" />
+      <div />
+    </>
+  );
+}
+
+export default App;
